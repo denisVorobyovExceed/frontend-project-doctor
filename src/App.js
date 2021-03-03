c82d1c7367a77eb88045c857a9784eb6abec939b
Denis <d.vorobyov.exceedteam@gmail.com>
2021-03-03 11:13:58 +0300

    init app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import './App.scss';
+
+function App() {
+  const [auth, setAuth] = useState(false);
+
+  return (
+    <div className="App">
+      {/* here we go */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/common/Header.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+function Header() {
+  const [title, setTitle] = useState('Зарегистрироваться в системе')
+
+  return (
+    <div></div>
+  )
+}
+
+export default Header;
